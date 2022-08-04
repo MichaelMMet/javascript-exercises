aecf1ff234a191be8b091cc4b45ba60645ebcc9a
Michael Metcalf <mikegmac14@gmail.com>
2022-08-03 20:44:47 -0700

    Add solutions for first and seccond exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(repeat, numTimes) {
+    let repeatedString = "";
+    if(numTimes >= 0){
+        for(let i = 0; i < numTimes; i++){
+            repeatedString += repeat;
+            }
+    } else {
+        return "ERROR"
+    }
 
+return repeatedString;
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
