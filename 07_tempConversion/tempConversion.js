9ef7af3df80fd888a7198e3bc4028366317a70d1
Michael Metcalf <mikegmac14@gmail.com>
2022-08-04 15:34:54 -0700

    Add solutions for exercise 07     used basic math along with Math.round and parseFloat to convert from     ferenheit to celcius and vice-versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,18 @@
-const ftoc = function() {
+const ftoc = function (fNum) {
+  let celcius = 0;
+  celcius = (fNum - 32) * (5 / 9);
+  celcius = Math.round(celcius * 10) / 10;
 
+  celcius = parseFloat(celcius.toFixed(1));
+  return celcius;
 };
 
-const ctof = function() {
-
+const ctof = function (cNum) {
+let farenheit = 0;
+farenheit = (cNum * (9/5)) + 32;
+farenheit = Math.round(farenheit * 10) / 10;
+farenheit = parseFloat(farenheit.toFixed(1));
+return farenheit;
 };
 
 // Do not edit below this line
