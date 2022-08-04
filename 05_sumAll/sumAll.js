704e26a0ebdce55c0b612518c8594ddcd8283b2e
Michael Metcalf <mikegmac14@gmail.com>
2022-08-04 14:46:40 -0700

    Add functionality for function to return error on cases of negative or non-integer variables     exercise completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,15 +1,19 @@
-const sumAll = function(startNum, endNum) {
+const sumAll = function (startNum, endNum) {
     let summedNum = 0;
-    if(startNum < endNum){
-        for(let i = startNum; i <= endNum; i++){
-            summedNum += i;
+    if ((startNum < 0 || endNum < 0) || (!Number.isFinite(startNum) || !Number.isFinite(endNum))) {
+        return "ERROR";
+    } else {
+        if (startNum < endNum) {
+            for (let i = startNum; i <= endNum; i++) {
+                summedNum += i;
             }
             return summedNum;
-    } else if(endNum < startNum){
-        for(let i = startNum; i >= endNum; i--){
-            summedNum += i;
+        } else if (endNum < startNum) {
+            for (let i = startNum; i >= endNum; i--) {
+                summedNum += i;
             }
             return summedNum;
+        }
     }
 
 };
